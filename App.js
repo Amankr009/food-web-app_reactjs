9b69e4a13baef9122a66a0bf691826d0efb85531
Aman Kumar <amakumar@qualys.com>
2024-02-09 01:57:01 +0530

    react config consists parcel, react, react-dom, start and build under script

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-const heading = React.createElement("h1",{id:"heading"},"Test React..");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//functional component
+const Title = () => (
+    <h1 className="heading">
+        Learn React..
+    </h1>
+);
+
+const MainComponent = () => (
+    <div className="main">
+        <Title />
+        <span className="info">Checking Functional Component in React..</span>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<MainComponent />);
