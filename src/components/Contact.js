7f4ff995b93bdbf40909624b14ac9f85288cacd8
Aman Kumar Yadav <40364495+Amankr009@users.noreply.github.com>
2024-02-29 00:12:02 +0530

    minor changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
             <div className="flex-col">
                 <div><input className="mt-2 border-2 border-gray-700 rounded" placeholder="Name" type="text" /></div>
                 <div><input className="mt-2 border-2 border-gray-700 rounded" placeholder="Messages" type="text" /></div>
-                <div><button className="px-2 m-2 border-2 bg-gray-800 rounded-lg text-white hover:bg-gray-500 active:bg-gray-700" onClick={window.alert("Request Submitted.📨")}>Submit</button></div>
+                <div><button className="px-2 m-2 border-2 bg-gray-800 rounded-lg text-white hover:bg-gray-500 active:bg-gray-700" onClick={()=> window.alert("Request Submitted.📨")}>Submit</button></div>
             </div>
         </div>
     )
